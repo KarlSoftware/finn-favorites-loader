275d70515067397fa454ed4c44b6876d0d12c1db
Nicolas Nordhagen <nicolas.nordhagen@eniro.com>
2017-11-07 08:27:03 +0100

    code cleanup

--- content.js
+++ content.js
@@ -1,43 +1,46 @@
-(function ($, undefined) {
-    $(function () {
-        var pathName = window.location.href;
+var pathName = window.location.href;
 
-        var numOfPages = $('.hidelt768 a:last-child').text();
-        $('div.listing > div.inner > div.bd > div:last-child').remove();
+// Gets the content of the last child in "pagination". Might be a better way to find the last page, but this works for now.
+var numOfPages = $('.hidelt768 a:last-child').text();
 
-        if (numOfPages > 1) {
+// Only run script content if more than 1 page. 
+if (numOfPages > 1) {
+    // Removes the whole pagination from the website. Ain't nobody got time to click that...
+    $('div.listing > div.inner > div.bd > div:last-child').remove();
 
-            var filler = [];
-            for (var i = 2; i <= numOfPages; i++) {
-                filler.push(i);
-            }
+    // Create array with all page Integers
+    var pageArr = [];
+    for (var i = 2; i <= numOfPages; i++) {
+        pageArr.push(i);
+    }
 
-            var listDiv = $('div.listing > div.inner > div.bd');
-            var divHeight = listDiv.innerHeight();
-            var wHeight = $(window).innerHeight();
+    // Save the listing div. 
+    var listDiv = $('div.listing > div.inner > div.bd');
+    var divHeight = listDiv.innerHeight();
+    var wHeight = $(window).innerHeight();
 
-            // to ensure only ONE call per additional page
-            var pause = false; 
+    // to ensure only ONE call per additional page
+    var pause = false; 
 
-            // Watch user scroll
-            $(window).scroll(function() {
-                if($(this).scrollTop() >= (divHeight - wHeight) && !pause && filler.length) {
-                    console.warn('end reached');
-                    var nextPage = filler.splice(0,1);
-                    pause = true;
+    // Watch user scroll
+    $(window).scroll(function() {
+        if($(this).scrollTop() >= (divHeight - wHeight) && !pause && pageArr.length) {
+            // Splice next page from array.
+            var nextPage = pageArr.splice(0,1);
+            // Pause scrolling. 
+            pause = true;
 
-                    var url = pathName + ("&page=" + nextPage);
+            // Generate URL for the page we want to fetch content from.
+            var url = pathName + ("&page=" + nextPage);
 
-                    $.get(url, function(data) {
-                        var content = $(data).find('div.listing > div.inner > div.bd');
-                        $(content).children('div:last').remove();
-                        listDiv.append($(content).html());
-                        divHeight = listDiv.innerHeight();
-                        pause = false;
-                    });
-                }
+            // Run a GET method for next page
+            $.get(url, function(data) {
+                var content = $(data).find('div.listing > div.inner > div.bd');
+                $(content).children('div:last').remove();
+                listDiv.append($(content).html());
+                divHeight = listDiv.innerHeight();
+                pause = false;
             });
-
-        }            
+        }
     });
-})(window.jQuery.noConflict(true));
+}
